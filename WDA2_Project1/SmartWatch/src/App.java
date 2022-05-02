19607fbcc227efb2dd44ad6075284374dc233101
LeeJeongMin <spdlqj7663@naver.com>
2022-05-02 16:55:36 +0900

    delete test 2

--- WDA2_Project1/SmartWatch/src/App.java
+++ WDA2_Project1/SmartWatch/src/App.java
@@ -4,8 +4,6 @@ public class App {
     public static void main(String[] args)  {
         WorldTime wd = new WorldTime();
         wd.set();
-        int a=4;
-        int b =5;
-        
+      
     }
 }
