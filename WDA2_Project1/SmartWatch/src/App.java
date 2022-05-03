39fb1bc75f97ae3a34ac75a7a2403144a251c926
Park Jun Hong <103193673+HONGBOY1@users.noreply.github.com>
2022-05-04 01:20:27 +0900

    Update project 5월04일

--- WDA2_Project1/SmartWatch/src/App.java
+++ WDA2_Project1/SmartWatch/src/App.java
@@ -1,9 +1,82 @@
-import WorldTime.WorldTime;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.plaf.TableUI;
 
-public class App {
+import java.awt.BorderLayout;
+
+import WorldTime.WorldTime;
+import Timer.Timer;
+import StopWatch.StopWatch;
+import Alarm.Alarm;
+
+import javafx.scene.paint.Color;
+
+public class App extends JFrame{
+
+    // 이동하는 버튼 변수
+    private JButton btnAlarm;
+    private JButton btnStopWatch;
+    private JButton btnTimer;
+    private JButton btnWorldTime;
+    
+    Color color = (Color.WHITE);
+
+    public App() {  // App  실행 함수
+    
+        WorldTime world = new WorldTime();   // 세계시간 메소드 불러 오기
+
+        btnAlarm = new JButton("알람");
+        btnStopWatch = new JButton("스톱워치");
+        btnTimer = new JButton("타이머");
+        btnWorldTime = new JButton("세계시간");
+        
+        JPanel pnlbtn = new JPanel(); // 버튼 패널 추가
+       
+        pnlbtn.add(btnWorldTime);
+        pnlbtn.add(btnTimer);
+        pnlbtn.add(btnAlarm);
+        pnlbtn.add(btnStopWatch);
+        
+        //위치 설정 
+        JPanel pnlSouth = new JPanel(new BorderLayout());
+        pnlSouth.add(pnlbtn, BorderLayout.SOUTH);
+
+        JPanel pnlMain = new JPanel(new BorderLayout());
+        pnlMain.add(world.lblCenter, BorderLayout.CENTER);
+        pnlMain.add(pnlSouth, BorderLayout.SOUTH);
+
+        add(pnlMain);
+
+        world.ThreadTime(); // 실행과 동시에 실행
+
+        // 버튼 클릭 이벤트
+        btnAlarm.addActionListener((e)->{
+            world.t1.interrupt(); 
+        });
+        btnStopWatch.addActionListener((e)->{
+            world.t1.interrupt();
+        });
+        btnTimer.addActionListener((e)->{
+            world.t1.interrupt();
+        });
+        btnWorldTime.addActionListener((e)->{
+            world.aa=true;
+            world.ThreadTime();
+        });
+        
+        // 스윙 창 띄우기
+        setTitle("Timer");
+        setSize(500,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        
+    }
+
     public static void main(String[] args)  {
-        WorldTime wd = new WorldTime();
-        wd.set();
-      
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new App();
+        
     }
 }
--- WDA2_Project1/SmartWatch/src/WorldTime/WorldTime.java
+++ WDA2_Project1/SmartWatch/src/WorldTime/WorldTime.java
@@ -1,17 +1,77 @@
 package WorldTime;
 
+import java.util.Calendar;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class WorldTime extends JFrame{
+    public boolean aa;
+    public Thread t1;
+    public JLabel lblCenter;
+
     public WorldTime(){
+            
+    }
+
+    public void ThreadTime() {
         
+        t1 = new Thread() {
+           public void run() {
+               
+               while(aa){
+               Calendar cal = Calendar.getInstance();
+                   
+                   // String으로 객체를 생성하는것보다
+                   // 메모리를 더 효율적으로 사용하기 위해서 StirngBuffer 사용
+                   
+                   StringBuffer now = new StringBuffer();
+                   
+                   now.append(cal.get(Calendar.HOUR_OF_DAY));
+                   now.append("시");
+                   now.append(cal.get(Calendar.MINUTE));                    
+                   now.append("분");
+                   now.append(cal.get(Calendar.SECOND));
+                   now.append("초");
+
+                   lblCenter.setText(now.toString());
+                              
+               try {
+                       Thread.sleep(1000); 
+               } catch(InterruptedException ie) {
+                   ie.printStackTrace();
+                   aa = false;
+               }
+           }
+               
+           }
+       };
+       t1.start();
+   }
+    /*    
+    public void timeget(){
+        while(aa){
+            Calendar t = Calendar.getInstance();
+            int year = t.get(Calendar.YEAR);        //연도를 받아온다.
+            int month = t.get(Calendar.MONTH);      //월를 받아온다.
+            int date = t.get(Calendar.DATE);        //일를 받아온다.
+            int day = t.get(Calendar.DAY_OF_MONTH); //요일을 가저온다
+            int amPm = t.get(Calendar.AM_PM);       //오전/오후 받아온다.
+            int hour = t.get(Calendar.HOUR);        //시를 받아온다.
+            int min = t.get(Calendar.MINUTE);       //분를 받아온다.
+            int sec = t.get(Calendar.SECOND);       //초를 받아온다.
+            int msec = t.get(Calendar.MILLISECOND); //msec를 리턴 그러나 적요하지 않음.
+            System.out.println(hour + "시"+ min +"분");
+            try{
+                Thread.sleep(100); // 0.1.초
+            } catch(InterruptedException ie) {
+ 
+                ie.printStackTrace();
+        
+                aa = false;
+            }
+        }
     }
-    public void set(){
-        setTitle("SmartWatch");
-        setSize(500,500);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+    */
 }
